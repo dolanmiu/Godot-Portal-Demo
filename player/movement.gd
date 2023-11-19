447cfce635dd2a9f83199b4cb817499b1ad4d160
Dolan Miu <dolan_miu@hotmail.com>
2023-11-19 13:13:59 +0000

    Add basic level

--- player/movement.gd
+++ player/movement.gd
@@ -1,47 +1,158 @@
 extends CharacterBody3D
 
-var _direction: Vector3 = Vector3.ZERO
-const _SENSITIVITY: int = 200
+@export_subgroup("Properties")
+@export var movement_speed = 5
+@export var jump_strength = 8
 
-# Called when the node enters the scene tree for the first time.
+@export_subgroup("Weapons")
+
+var mouse_sensitivity = 700
+var gamepad_sensitivity := 0.075
+
+var mouse_captured := true
+
+var movement_velocity: Vector3
+var rotation_target: Vector3
+
+var input_mouse: Vector2
+
+var health:int = 100
+var gravity := 0.0
+
+var previously_floored := false
+
+var jump_single := true
+var jump_double := true
+
+var container_offset = Vector3(1.2, -1.1, -2.75)
+
+var tween:Tween
+
+signal health_updated
+
+@onready var camera = $Camera
+
+@export var crosshair:TextureRect
+
+# Functions
+
 func _ready():
-	pass # Replace with function body.
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	
+	# Handle functions
+	
+	handle_controls(delta)
+	handle_gravity(delta)
+	
+	# Movement
+
+	var applied_velocity: Vector3
+	
+	movement_velocity = transform.basis * movement_velocity # Move forward
+	
+	applied_velocity = velocity.lerp(movement_velocity, delta * 10)
+	applied_velocity.y = -gravity
+	
+	velocity = applied_velocity
+	move_and_slide()
+	
+	# Rotation
+	
+	camera.rotation.z = lerp_angle(camera.rotation.z, -input_mouse.x * 25 * delta, delta * 5)	
+	
+	camera.rotation.x = lerp_angle(camera.rotation.x, rotation_target.x, delta * 25)
+	rotation.y = lerp_angle(rotation.y, rotation_target.y, delta * 25)
+	
+	
+	# Movement sound
+	
+	# Landing after jump or falling
+	
+	camera.position.y = lerp(camera.position.y, 0.0, delta * 5)
+	
+	if is_on_floor() and gravity > 1 and !previously_floored: # Landed
+		$LandSound.play()
+		camera.position.y = -0.1
+	
+	previously_floored = is_on_floor()
+	
+	# Falling/respawning
+	
+	if position.y < -10:
+		get_tree().reload_current_scene()
+
+# Mouse movement
 
 func _input(event):
-	if not event is InputEventKey:
-		return
+	if event is InputEventMouseMotion and mouse_captured:
 		
-	if event.is_echo():
-		return
-
-	if event.is_action_pressed("move_up"):
-		_direction.z -= 1
-		print('pressed')
-	if event.is_action_released("move_up"):
-		_direction.z += 1
-
-	if event.keycode == KEY_A:
-		if event.pressed:
-			_direction.x -= 1
-		else:
-			_direction.x += 1
-
-	if event.keycode == KEY_S:
-		if event.pressed:
-			_direction.z += 1
-		else:
-			_direction.z -= 1
-
-	if event.keycode == KEY_D:
-		if event.pressed:
-			_direction.x += 1
-		else:
-			_direction.x -= 1
-	print(_direction)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	velocity = _direction * delta * _SENSITIVITY
-
+		input_mouse = event.relative / mouse_sensitivity
 		
-	move_and_slide()
-	pass
+		rotation_target.y -= event.relative.x / mouse_sensitivity
+		rotation_target.x -= event.relative.y / mouse_sensitivity
+
+func handle_controls(_delta):
+	
+	# Mouse capture
+	
+	if Input.is_action_just_pressed("mouse_capture"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		mouse_captured = true
+	
+	if Input.is_action_just_pressed("mouse_capture_exit"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		mouse_captured = false
+		
+		input_mouse = Vector2.ZERO
+	
+	# Movement
+	
+	var input := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	
+	movement_velocity = Vector3(input.x, 0, input.y).normalized() * movement_speed
+	
+	# Rotation
+	
+	var rotation_input := Input.get_vector("camera_right", "camera_left", "camera_down", "camera_up")
+	
+	rotation_target -= Vector3(-rotation_input.y, -rotation_input.x, 0).limit_length(1.0) * gamepad_sensitivity
+	rotation_target.x = clamp(rotation_target.x, deg_to_rad(-90), deg_to_rad(90))
+	
+	# Jumping
+	
+	if Input.is_action_just_pressed("jump"):
+		
+		if jump_single or jump_double:
+			pass
+		
+		if jump_double:
+			
+			gravity = -jump_strength
+			jump_double = false
+			
+		if(jump_single): action_jump()
+		
+	# Weapon switching
+
+# Handle gravity
+
+func handle_gravity(delta):
+	
+	gravity += 20 * delta
+	
+	if gravity > 0 and is_on_floor():
+		
+		jump_single = true
+		gravity = 0
+
+# Jumping
+
+func action_jump():
+	
+	gravity = -jump_strength
+	
+	jump_single = false;
+	jump_double = true;
